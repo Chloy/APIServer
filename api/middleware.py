4937c23322596e0f91a0e10675427cf5b7d9330d
Hella <krisbeaky@gmail.com>
2022-06-25 18:18:58 +0500

    Add custom middleware

--- api/middleware.py
+++ api/middleware.py
@@ -1,11 +1,36 @@
+from __future__ import annotations
 from aiohttp import web
-from aiohttp_security.abc import AbstractAuthorizationPolicy
 
 
-class CustomAuthorization(AbstractAuthorizationPolicy):
-    async def authorized_userid(self, identity):
-        print(identity)
-        return await super().authorized_userid(identity)
+async def handle_404(request: web.Request) -> web.Response:
+    return web.Response(text='Error 404')
 
-    async def permits(self, identity, permission, context=None):
-        return await super().permits(identity, permission, context)
+
+async def handle_500(request: web.Request) -> web.Response:
+    return web.Response(text='Error 500')
+
+
+def create_error_middleware(override: dict[int, str]):
+
+    @web.middleware
+    async def error_middleware(request: web.Request, handler):
+        try:
+            return await handler(request)
+        except web.HTTPException as ex:
+            code = override.get(ex.status)
+            if code:
+                return await code(request)
+            raise
+        except Exception:
+            request.protocol.logger.exception('Error handling exception')
+            return await override[500](request)
+
+    return error_middleware
+
+
+def setup_middleware(app: web.Application) -> None:
+    error_middleware = create_error_middleware({
+        404: handle_404,
+        500: handle_500
+    })
+    app.middlewares.append(error_middleware)
--- entry.py
+++ entry.py
@@ -2,14 +2,14 @@ from aiohttp import web
 from aiohttp_security import setup, SessionIdentityPolicy
 from api import create_app
 from aiohttp_swagger import setup_swagger
-from api import middleware
-from api import models
+from api.middleware import setup_middleware
+from api.models import get_engine
 
 
 app = create_app()
-setup(app, SessionIdentityPolicy(), middleware.CustomAuthorization())
 setup_swagger(app, swagger_url="/api/v1/doc", ui_version=2)
-app.cleanup_ctx.append(models.get_engine)
+app.cleanup_ctx.append(get_engine)
+setup_middleware(app)
 
 
 if __name__ == '__main__':
